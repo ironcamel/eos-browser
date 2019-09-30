3d53c7266eaa1e35fc6a97f5f82fbd191acd3bbf
Naveed Massjouni <naveed@vt.edu>
2019-09-30 05:26:53 -0400

    eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-console': 'off',
+    'no-param-reassign': 'off',
+    'react/state-in-constructor': 'off',
+    'no-plusplus': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'no-continue': 'off',
+    'no-await-in-loop': "off",
+  },
+};
