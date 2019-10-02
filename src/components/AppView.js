7253e1e89f4b9fbe45e35118fbc0fff56fbfcfc7
Naveed Massjouni <naveed@vt.edu>
2019-10-01 23:08:19 -0400

    Add more options for num blocks to download

--- src/components/AppView.js
+++ src/components/AppView.js
@@ -33,6 +33,8 @@ function AppView(props) {
         <option value="2">2</option>
         <option value="5">5</option>
         <option value="10">10</option>
+        <option value="15">15</option>
+        <option value="20">20</option>
         <option value="25">25</option>
       </select>
       blocks
