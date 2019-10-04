bd64b3eb79e2ed9f67f2762a16de338f19a7e2d6
Naveed Massjouni <naveed@vt.edu>
2019-10-04 00:29:18 -0400

    Add App tests

---
+++ src/containers/App.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { createStore } from 'redux';
+import App from './App';
+import AppView from '../components/AppView';
+import reducer from '../reducers';
+
+const store = createStore(reducer);
+
+test('App properly connects to AppView props', () => {
+  const app = shallow(
+    <App eosClient={{ client: 'foo' }} store={store} />
+  );
+  const appView = app.find(AppView);
+  expect(appView).toHaveLength(1);
+  expect(appView.props()).toMatchObject({
+    eosClient: { client: 'foo' },
+    totalBlocks: 10,
+    isFetchingBlocks: false,
+    blocks: [],
+    blocksById: {},
+    requestBlocks: expect.any(Function),
+    setTotalBlocks: expect.any(Function),
+    dispatch: expect.any(Function),
+    store,
+  });
+});
--- src/containers/Block.test.js
+++ src/containers/Block.test.js
@@ -4,20 +4,22 @@ import { createStore } from 'redux';
 import Block from './Block';
 import BlockView from '../components/BlockView';
 import reducer from '../reducers';
-import { requestDetails, toggleDetails } from '../actions';
 
 const store = createStore(reducer);
 
 test('Block properly connects to BlockView props', () => {
   const blockData = { id: '100' };
   const block = shallow(
-      <Block block={blockData} eosClient={{}} store={store} />
+    <Block block={blockData} eosClient={{ client: 'foo' }} store={store} />
   );
   const blockView = block.find(BlockView);
   expect(blockView).toHaveLength(1);
-  const props = blockView.props();
-  expect(props.block).toEqual(blockData);
-  expect(typeof props.requestDetails).toEqual('function');
-  expect(typeof props.toggleDetails).toEqual('function');
-  expect(typeof props.dispatch).toEqual('function');
+  expect(blockView.props()).toMatchObject({
+    block: { id: '100' },
+    eosClient: { client: 'foo' },
+    toggleDetails: expect.any(Function),
+    requestDetails: expect.any(Function),
+    dispatch: expect.any(Function),
+    store,
+  });
 });
